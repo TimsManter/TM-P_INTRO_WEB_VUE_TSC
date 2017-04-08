1f54b5e14e5d70afb12c8f7c562865e236717ee4
Krzysztof 'TimsManter' Ferenc <timsmanter@gmail.com>
2017-04-09 00:02:25 +0200

    Updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,26 @@ const gulp = require('gulp');
 const source = require('vinyl-source-stream');
 const uglify = require('gulp-uglify');
 const streamify = require('gulp-streamify');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
 const pump = require('pump');
 
-gulp.task('default', function (cb) {
+gulp.task('default', ['js', 'css']);
+
+gulp.task('js', function (cb) {
     pump([
         browserify(['./js/script.js']).bundle(),
         source('script.min.js'),
         streamify(uglify()),
+        gulp.dest('./dist/')
+    ], cb);
+});
+
+gulp.task('css', function(cb) {
+    pump([
+        gulp.src('./scss/style.scss'),
+        sass({outputStyle: 'compressed'}).on('error', sass.logError),
+        rename('style.min.css'),
         gulp.dest('./dist/')
     ], cb);
 });
