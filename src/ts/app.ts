a46de3c6a26b1632dede0721ce9cdd589392d0ca
Krzysztof 'TimsManter' Ferenc <timsmanter@gmail.com>
2017-05-19 20:50:05 +0200

    Add router

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,20 +1,35 @@
+// Imports
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import VueMaterial from 'vue-material'
 import Component from 'vue-class-component'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
 import './../scss/style.scss'
 
+// Components
 import App from './vue/App.vue'
+import Projects from './vue/pages/Projects.vue'
 
 Axios.defaults.baseURL = 'https://api.github.com'
 
+Vue.use(VueRouter)
 Vue.use(VueMaterial)
 Vue.use(VueAxios)
 Vue.use(Axios)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/projects',
+      component: Projects
+    }
+  ]
+})
+
 new Vue({
   el: '#app',
+  router: router,
   render: r => r(App, {
-
   })
 })
