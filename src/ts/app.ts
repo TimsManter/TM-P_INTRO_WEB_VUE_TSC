5d44eec5d4174f41f122285280e0515ea51970be
Krzysztof 'TimsManter' Ferenc <timsmanter@gmail.com>
2017-05-19 20:30:14 +0200

    Add Axios for Ajax requests

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import Component from 'vue-class-component'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
 import './../scss/style.scss'
 
 import App from './vue/App.vue'
 
-Vue.use(VueMaterial);
+Axios.defaults.baseURL = 'https://api.github.com'
+
+Vue.use(VueMaterial)
+Vue.use(VueAxios)
+Vue.use(Axios)
 new Vue({
   el: '#app',
   render: r => r(App, {
