6679ff264f1a65f69f54528cd8f6505e5696b15e
Krzysztof 'TimsManter' Ferenc <timsmanter@gmail.com>
2017-05-20 14:06:11 +0200

    Remove unnecessary wrapper

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,7 +4,6 @@ import VueRouter from 'vue-router'
 import VueMaterial from 'vue-material'
 import Component from 'vue-class-component'
 import Axios from 'axios'
-import VueAxios from 'vue-axios'
 import './../scss/style.scss'
 
 // Components
@@ -16,7 +15,6 @@ Axios.defaults.baseURL = 'https://api.github.com'
 
 Vue.use(VueRouter)
 Vue.use(VueMaterial)
-Vue.use(VueAxios, Axios)
 
 const router = new VueRouter({
   routes: [
