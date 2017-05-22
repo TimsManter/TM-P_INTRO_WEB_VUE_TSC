f6504bcbd5b383eeb762531c1f22df5f4abfd6e9
TimsManter <timsmanter@gmail.com>
2017-05-22 03:06:35 +0200

    Make card area flex

--- src/ts/vue/elements/ProjectCardItem.vue
+++ src/ts/vue/elements/ProjectCardItem.vue
@@ -1,10 +1,17 @@
 <template>
-  <md-card>
-    <md-card-header>
-      <div class="md-title">{{ repo.name }}</div>
-      <div class="md-subhead">{{ repo.description }}</div>
-    </md-card-header>
-  </md-card>
+  <md-layout md-flex="33" md-flex-small="50" md-flex-xsmall="100">
+    <md-card>
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">{{ repoName }}</div>
+          <div class="md-subhead">{{ repo.description }}</div>
+        </md-card-header-text>
+        <md-card-media>
+          <md-icon md-iconset="mdi mdi-school" class="md-size-3x"></md-icon>
+        </md-card-media>
+      </md-card-header>
+    </md-card>
+  </md-layout>
 </template>
 
 <script lang="ts">
@@ -18,5 +25,27 @@
     }
   })
   export default class extends Vue {
+    repo: Object
+
+    get repoNameSections() {
+      return (this.repo as any).name.split('_')
+    }
+
+    get repoName() {
+      let name: String = this.repoNameSections[1]
+      if (name == null) return this.repoNameSections[0]
+      else return name
+    }
+
+    get repoType() {
+      return this.repoNameSections.split('-')[1]
+    }
   }
 </script>
+
+<style lang="scss">
+  .md-card {
+    width: 100%;
+    margin-bottom: 20px;
+  }
+</style>
