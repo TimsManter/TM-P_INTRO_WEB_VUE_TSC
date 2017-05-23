823e45fff19c6b4aa8992d8c8d47188d2d1663c7
TimsManter <timsmanter@gmail.com>
2017-05-23 17:05:44 +0200

    Split config file

---
+++ webpack.dev.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: "./src/ts/app.ts",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'app.min.js'
+  },
+  resolve: {
+    extensions:
+    [
+      '.js',
+      '.scss',
+      '.css',
+      '.ts',
+      '.vue'
+    ],
+    modules:
+    [
+      'node_modules',
+      path.resolve(__dirname, 'src')
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src/ts'),
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
+      {
+        test: /\.(svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.vue$/,
+        include: path.resolve(__dirname, 'src/ts/vue'),
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+              alias: {
+                '../fonts': 'mdi/fonts'
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
