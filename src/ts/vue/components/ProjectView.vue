c551db60c162d212fce119d68e6daf92b669a252
TimsManter <timsmanter@gmail.com>
2017-05-28 14:02:02 +0200

    Cleanup code

--- src/ts/vue/components/ProjectView.vue
+++ src/ts/vue/components/ProjectView.vue
@@ -21,26 +21,5 @@
     }
   })
   export default class ProjectsView extends Vue {
-
-    // mounted() {
-    //   (this.$refs['readmeTab'] as any).$on('readmeTab', () => {
-    //     this.simulateClick()
-    //   })
-    // }
-
-    // simulateClick() {
-    //   var evt = document.createEvent("MouseEvents");
-    //   evt.initMouseEvent("click", true, true, window,
-    //     0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    //   var bt = document.getElementById('readmeTab').parentNode.parentNode.parentNode.firstChild.firstChild
-    //   var canceled = !bt.dispatchEvent(evt);
-    //   if(canceled) {
-    //     // A handler called preventDefault
-    //     alert("canceled");
-    //   } else {
-    //     // None of the handlers called preventDefault
-    //     alert("not canceled");
-    //   }
-    // }
   }
 </script>
