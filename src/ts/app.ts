96ba86ea98046e959d57f236249343ea2b050863
TimsManter <timsmanter@gmail.com>
2017-06-07 10:59:01 +0200

    Update format to meet rules

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,62 +1,62 @@
 // Imports
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueMaterial from 'vue-material'
-import Axios from 'axios'
-import './../scss/style.scss'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import VueMaterial from "vue-material";
+import Axios from "axios";
+import "./../scss/style.scss";
 
 // Components
-import App from '../vue/App.vue'
-import ProjectsPage from '../vue/pages/ProjectsPage.vue'
-import StartPage from '../vue/pages/StartPage.vue'
+import App from "../vue/App.vue";
+import ProjectsPage from "../vue/pages/ProjectsPage.vue";
+import StartPage from "../vue/pages/StartPage.vue";
 
-Axios.defaults.baseURL = 'https://api.github.com'
-Axios.defaults.headers.common['Accept'] = 'application/vnd.github.v3+json'
-Axios.defaults.headers.common['Authorization'] = 'token a0a02a08500c5b45844a99d265a45a5328e853a8'
+Axios.defaults.baseURL = "https://api.github.com";
+Axios.defaults.headers.common["Accept"] = "application/vnd.github.v3+json";
+Axios.defaults.headers.common["Authorization"] = "token a0a02a08500c5b45844a99d265a45a5328e853a8";
 
 Vue.use(VueRouter);
 Vue.use(VueMaterial);
 
-(Vue as any).material.registerTheme('default', {
+(Vue as any).material.registerTheme("default", {
   primary: {
-    color: 'cyan',
+    color: "cyan",
     hue: 700
   },
   accent: {
-    color: 'green',
+    color: "green",
     hue: 500
   },
   warn: {
-    color: 'red',
+    color: "red",
     hue: 500
   },
   background: {
-    color: 'white',
+    color: "white",
     hue: 500
   }
-})
+});
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
-      path: '/projects',
+      path: "/projects",
       component: ProjectsPage
     },
     {
-      path: '/start',
+      path: "/start",
       component: StartPage
     },
     {
-      path: '/',
-      redirect: '/projects'
+      path: "/",
+      redirect: "/projects"
     }
   ]
-})
+});
 
-new Vue({
-  el: '#app',
+const app = new Vue({
+  el: "#app",
   router: router,
   render: r => r(App, {
   })
-})
+});
